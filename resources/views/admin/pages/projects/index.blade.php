f0181ba0888b9e95f1c4ec017d3f3f61288fb578
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-28 15:27:53 +0200

    Style Review Index (page)

--- resources/views/admin/pages/projects/index.blade.php
+++ resources/views/admin/pages/projects/index.blade.php
@@ -2,36 +2,45 @@
 
 @section('content')
 <div class='container d-flex justify-content-between align-items-start flex-wrap mt-5'>
-    @foreach ($projects as $key=>$el)
-        {{-- @php dd($el) @endphp --}}
 
-            <div class="card-index card p-3 mx-2 mb-5" style="width:calc(95% / 2); height: 35rem">
-                <a href="{{ route('admin.project.show',$el) }}" style="text-decoration:none;">
-                    <img src="{{asset('storage/'. $el->cover_image)}}" class="card-img-top" alt="{{$el->title . 'cover'}}">
-                    <div class="card-body">
-                        <h2 class="card-title mb-3 text-center text-white">{{ $el->title }}</h2>
-                        {{-- technologies  --}}
-                        @if($el->technologies)
-                            <div class="my-3 text-center">
-                                @foreach ($el->technologies as $el)
-                                    <span class="badge rounded-pill text-bg-light">{{ $el->name }}</span>        
-                                @endforeach
-                            </div>
-                        @endif
-                        <h6 class="card-subtitle mt-0 mb-2 text-body-secondary text-center">Project numero: {{ $key + 1}}</h6>
-                        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="text-decoration-underline">Type</span>:
-                        @if($el->type)
-                            {{ $el->type->name_type }}
-                        @else
-                            none
-                        @endif
-                        </h6>
-                        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="text-decoration-underline">Description</span>: {{ $el->description }}</h6>
-                        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="text-decoration-underline">Buyer</span>: {{ $el->buyer }}</h6>
-                        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="text-body-secondary">Link:</span><a href="{{$el->link}}" target="_blank" class="ms-1 card-link">Click here!</a></h6>
+    <table class="table table-dark table-hover">
+        <thead>
+          <tr>
+            <th scope="col">n°</th>
+            <th scope="col">Project Name</th>
+            <th scope="col">Type of Project</th>
+            <th scope="col">Project Link</th>
+            <th scope="col">Go</th>
+            <th scope="col">Technologies</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach ($projects as $key=>$el)
+            <tr>
+                <th scope="row" class="align-middle">{{$key + 1}}</th>
+                <td class="align-middle">{{ $el->title }}</td>
+                <td class="align-middle">{{ $el->type->name_type }}</td>
+                <td class="align-middle">
+                    <a href="{{$el->link}}" target="_blank"> 
+                        Click here!
+                    </a>
+                </td>
+                <td class="align-middle">
+                    <a href="{{ route('admin.project.show',$el) }}" type="button" class="btn btn-outline-success">To Project</a>
+                </td>
+                <td class="align-middle">
+                {{-- technologies  --}}
+                @if($el->technologies)
+                    <div class="my-3">
+                        @foreach ($el->technologies as $el)
+                            <span class="badge rounded-pill text-bg-light">{{$el->name }}</span>        
+                        @endforeach
                     </div>
-                </a>
-            </div>
-    @endforeach
+                @endif
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
